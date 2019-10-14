c80f0d037ce2451d3641f5831b9d4f73a501536e
diegonavarro96 <diegonavarroarizpe@hotmail.com>
2019-10-14 17:48:30 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 
 
 const express = require('express')
+const omdb =require('./movies.js')
 
 const app = express()
 
@@ -19,32 +20,75 @@ const app = express()
 
 
 
-app.get('/',function(req, res){
-    res.send('Hola mundo!')
-})
 
 
-app.get('/about',function(req,res){
-    res.send('un about muy interesenante')
-})
 
-app.get('/misc',function(req,res){
-    res.send({
-        dia : 'jueves',
-        description: 'casi Viernes'
-    })
-})
+//app.get('/about',function(req,res){
+  //  res.send('un about muy interesenante')
+//})
+
+//app.get('/misc',function(req,res){
+  //  res.send({
+    //    dia : 'jueves',
+     //   description: 'casi Viernes'
+    //})
+//})
 
 // Prender el servidor:
 // 
 
-app.listen(3000,function(){
-    console.log('up and running!')
+
+app.get('/omdb', function (req, res) {
+    if (!req.query.search) {
+        res.send({
+            error: "Debes de enviar un titulo de una pelicula o serie"
+        })
+    }
+
+    omdb.omdbMovie(req.query.search, function (error, response) {
+        if (error) {
+            return response.send({
+                error: error
+            })
+        }
+        
+        if (response.season) {
+            var title = response.title
+            omdb.omdbSeason(response.title, response.season, function (error, response) {
+                if (error) {
+                    return response.send({
+                        error: error
+                    })
+                }
+                response.send({
+                    title: title,
+                    season: response.season,
+                    episodes: response.episodes
+                })
+                
+            })
+
+        } 
+        res.send(response)
+    })
+})
+
+
+
+app.get('/',function(req, res){
+    res.send({
+        greeting: 'Hola mundo!'
+    })
 })
 
 
 
 // no section found
 app.get('*',function(req,res){
-    res.send('La cagaste')
+    res.send({
+        error: 'Ruta no valida!'})
+})
+
+app.listen(3000,function(){
+    console.log('up and running!')
 })
